4d31d28647a0bd9ef69717ae50c77902b628a4dc
Samuel Alves <samueltheodoro21@gmail.com>
2023-03-16 20:27:31 -0300

    deploy for test

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const general = () => {}
+const general = () => {
+  const sections = document.querySelectorAll('.sections')
+
+  sections.forEach((section) => {
+    section.style.height = `${window.innerHeight}px`
+  })
+
+  const handleResize = () => {
+    sections.forEach((section) => {
+      section.style.height = `${window.innerHeight}px`
+    })
+  }
+
+  window.onresize = handleResize
+}
 
 general()
